330e90e389b312a842539e36a1c647d16bfdb9d7
dev_johsire <josiah5115@gmail.com>
2019-02-28 02:42:06 -0700

    Chat App Initiate

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Build a simple chatting app using Socket.io | Html | Express | Node
+
+Client(user) and Server(app) real time communication.
---
+++ server.js
@@ -0,0 +1,7 @@
+// Our Express Server;
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
